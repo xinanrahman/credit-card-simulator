d8ce390f9d12ce3d1c3e2583ca26f36f35879f93
Xinan Rahman <xinanrahman03@gmail.com>
2023-11-23 16:52:46 -0500

    feat: implemented backend route for viewing card balances

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import { api } from "~/utils/api";
 
 export default function Home() {
   const hello = api.cardActions?.hello.useQuery({ text: "from tRPC" });
+  const { data, isLoading, error } = api.cardViews.getBalances.useQuery();
 
   return (
     <>
@@ -14,9 +15,21 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <UserButton afterSignOutUrl="/"></UserButton>
-      <div className="bg-primary-foreground flex min-h-screen flex-col items-center justify-center">
-        <p className="text-foreground text-2xl">
+      <div className="flex min-h-screen flex-col items-center justify-center bg-primary-foreground">
+        <p className="text-2xl text-foreground">
           {hello.data ? hello.data.greeting : "Loading tRPC query..."}
+        </p>
+        <p className="text-2xl text-foreground">
+          Available Balance:{" "}
+          {data?.availableBalance
+            ? data.availableBalance
+            : "Loading available balance..."}
+        </p>
+        <p className="text-2xl text-foreground">
+          Payable Balance:{" "}
+          {data?.payableBalance
+            ? data.availableBalance
+            : "Loading payable balance..."}
         </p>
       </div>
     </>
--- src/server/api/routers/cardViews.ts
+++ src/server/api/routers/cardViews.ts
@@ -3,8 +3,29 @@ import { z } from "zod";
 import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
 
 export const cardViewsRouter = createTRPCRouter({
-  getBalances: protectedProcedure.query(({ ctx }) => {
-    // TODO: get available balance and payable balance from database using Prisma
+  getBalances: protectedProcedure.query(async ({ ctx }) => {
+    const userId = ctx.auth.userId;
+    if (!userId) {
+      throw new Error("User is not authenticated");
+    }
+
+    let balance = await ctx.db.balance.findUnique({
+      where: {
+        id: userId,
+      },
+    });
+
+    if (!balance) {
+      balance = await ctx.db.balance.create({
+        data: {
+          id: userId,
+        },
+      });
+    }
+    return {
+      availableBalance: balance.availableBalance,
+      payableBalance: balance.payableBalance,
+    };
   }),
   getTransactions: protectedProcedure.query(({ ctx }) => {
     // TODO: get pending and settled transactions from database using Prisma
