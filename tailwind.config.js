a8890eb5d11a474dfa0b7f2feb496362c64718de
Xinan Rahman <xinanrahman03@gmail.com>
2023-11-25 19:18:46 -0500

    chore: scaffolded front-end using next-ui

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+import { nextui } from "@nextui-org/react";
+
+/** @type {import('tailwindcss').Config} */
+const config = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  darkMode: "class",
+  plugins: [nextui()],
+};
+
+export default config;
